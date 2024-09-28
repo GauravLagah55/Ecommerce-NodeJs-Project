29bdcf63a033615f527d646d19e9294d0e49aa9b
GauravLagah55 <lagahgaurav55@gmail.com>
2024-09-28 16:09:46 +0530

    Collections
    environment and apis

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/NodeProject")
+mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce-Nodejs-Project")
 .then(()=>{
     console.log("Database Connected");
 })
