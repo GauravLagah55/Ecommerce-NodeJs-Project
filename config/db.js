141134526b7848bf2668faf65ec1e46c6b67c376
GauravLagah55 <lagahgaurav55@gmail.com>
2024-12-20 12:41:10 +0530

    vercel

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce-Nodejs-Project")
+mongoose.connect("mongodb+srv://lagahgaurav55:j5I9P9bzzriQMtYy@cluster0.zygxr.mongodb.net/Ecommerce-Nodejs-Project")
 .then(()=>{
     console.log("Database Connected");
 })
